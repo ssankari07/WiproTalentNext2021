cc3739ab2d461e8384f1012d1640aab246f0cc98
sivasankari <59524367+ssankari07@users.noreply.github.com>
2020-05-29 09:50:02 +0530

    Create sum of last two digits.java

---
+++ mettl/programs/day3/sum of last two digits.java
@@ -0,0 +1,11 @@
+import java.io.*; 
+import java.util.*;
+class UserMainCode
+{
+public int addLastDigits(int input1, int input2){
+if(input<0)
+input1=(-1)*input1;
+if(input2=(-1)*input2;
+return(input1%10)+(input2%10);
+}
+}
