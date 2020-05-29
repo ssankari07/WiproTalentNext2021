a9892a71e0af92af9ef6195e5eed6f53be35fc55
sivasankari <59524367+ssankari07@users.noreply.github.com>
2020-05-29 14:41:24 +0530

    Create arguments or not.java

---
+++ flowcontrol/arguments or not.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=args.length;
+		if(a==0){
+		    System.out.println("No Values");
+		}
+		else{
+		   System.out.println(arg[0]);
+		   for(i=1;i<k;i++){
+		    System.out.println(","+args[i]);
+		}
+	}
+}
+}
