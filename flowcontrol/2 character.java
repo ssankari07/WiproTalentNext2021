741ccb4178a68d979bec0afb899be69f60afd1fd
sivasankari <59524367+ssankari07@users.noreply.github.com>
2020-05-29 15:09:21 +0530

    Create 2 character.java

---
+++ flowcontrol/2 character.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+public static void main(String[] args)
+{
+Scanner s = new Scanner ( System .in);
+
+System.out.println("enter the first character");
+char ch1=s.next().charAt(0);
+
+System.out.println("enter the second character");
+char ch2=s.next().charAt(0);
+
+if (ch1>ch2)
+System.out.println(ch2+" , "+ch1);
+
+else
+System.out.println(ch1+" , "+ch2);
+}
