4976119319df642aa4d4c8e227bf834a65fc9d7a
sivasankari <59524367+ssankari07@users.noreply.github.com>
2020-05-30 10:19:37 +0530

    Create sum of numbers.java

---
+++ HackerRank/day5/sum of numbers.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+class Result {
+
+    public static int arraySum(List<Integer> numbers) {
+      int sum=0;
+      for(int i:numbers)
+      {
+          sum+=i;
+      }
+      return sum;
+
+      }
+
+      }
